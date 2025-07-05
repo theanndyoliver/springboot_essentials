518f7e923f40b64be9823d503a7f967adced21db
Anderson de Oliveira <adm-loren@hotmail.com>
2025-07-05 09:46:23 -0400

    Implements Function WebMvcConfigurer

--- src/main/java/academy.devdojo.springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy.devdojo.springboot2/requests/AnimePostRequestBody.java
@@ -1,10 +1,8 @@
 package academy.devdojo.springboot2.requests;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.hibernate.validator.constraints.URL;
 
 @Data
 @AllArgsConstructor
