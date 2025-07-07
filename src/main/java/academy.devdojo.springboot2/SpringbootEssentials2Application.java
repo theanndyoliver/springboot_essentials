28928482b8fb5e98d60b78af7e07c7ef4057e937
Anderson de Oliveira <adm-loren@hotmail.com>
2025-07-06 23:55:50 -0400

    Update SpringCliente.main - RestTemplate PUT and DELETE

--- src/main/java/academy.devdojo.springboot2/SpringbootEssentials2Application.java
+++ src/main/java/academy.devdojo.springboot2/SpringbootEssentials2Application.java
@@ -2,7 +2,6 @@ package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringbootEssentials2Application {
--- src/main/java/academy.devdojo.springboot2/client/SpringClient.java
+++ src/main/java/academy.devdojo.springboot2/client/SpringClient.java
@@ -1,15 +1,18 @@
 package academy.devdojo.springboot2.client;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.mapper.AnimeMapper;
+import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
 
+@ResponseBody
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
@@ -28,8 +31,36 @@ public class SpringClient {
         log.info(exchange.getBody());
 
 
+      //  Anime kingdom = Anime.builder().name("kingdom").build();
+       // Anime savedAnime =new RestTemplate().postForObject("http://localhost:8080/animes",kingdom,Anime.class);
+       // log.info("saved anime {}",savedAnime);
 
 
+        //Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+       // ResponseEntity<Anime> samuraiChamplooSaved =new RestTemplate().exchange("http://localhost:8080/animes",HttpMethod.POST,
+               // new HttpEntity<>(samuraiChamploo,createJsonHeader()),Anime.class);
+        //log.info("saved anime {}",samuraiChamplooSaved);
+
+
+
+        Anime bokuNoHero = Anime.builder().id(1302l).name("Boku no Hero - Temporada").build();
+
+        ResponseEntity<Void> bokuNoHeroUpdate = new RestTemplate().exchange("http://localhost:8080/animes",HttpMethod.PUT,
+                new HttpEntity<>(bokuNoHero,createJsonHeader()),Void.class);
+        log.info(bokuNoHeroUpdate);
+
+
+        Anime deleteAnimeXBB = Anime.builder().id(1352l).build();
+        ResponseEntity<Void> animeDeleteXD = new RestTemplate().exchange("http://localhost:8080/animes/remove/{id}",HttpMethod.DELETE,
+                null,Void.class,deleteAnimeXBB.getId());
+        log.info(animeDeleteXD);
+
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 
 }
