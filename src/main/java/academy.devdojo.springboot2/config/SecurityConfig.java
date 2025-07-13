902e491826cfb8d5743800ce4ceeea8a6736ea5d
Anderson de Oliveira <adm-loren@hotmail.com>
2025-07-12 21:00:53 -0400

    SpringSecurity pt 2 - CSRF

--- src/main/java/academy.devdojo.springboot2/config/SecurityConfig.java
+++ src/main/java/academy.devdojo.springboot2/config/SecurityConfig.java
@@ -3,10 +3,15 @@ package academy.devdojo.springboot2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,22 +19,26 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig  {
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http
-                    .authorizeHttpRequests((authz) -> authz
-                            .anyRequest().authenticated()
-                    )
-                    .httpBasic(withDefaults());
-            return http.build();
-        }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csfr -> csfr.disable())
+                //.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
+        UserDetails user = User.builder()
                 .username("admin")
-                .password("1234")
-                .roles("USER","ADMIN")
+                .password(encoder.encode("1234"))
+                .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
