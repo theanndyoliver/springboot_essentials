46c2cfe5440909adc9fdb68092ff802c76c78bd8
Anderson de Oliveira <adm-loren@hotmail.com>
2025-07-31 11:12:23 -0400

    Grafana

--- src/main/java/academy.devdojo.springboot2/SpringbootEssentials2Application.java
+++ src/main/java/academy.devdojo.springboot2/SpringbootEssentials2Application.java
@@ -1,7 +1,11 @@
 package academy.devdojo.springboot2;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootEssentials2Application {
@@ -9,6 +13,12 @@ public class SpringbootEssentials2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEssentials2Application.class, args);
 
+
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
